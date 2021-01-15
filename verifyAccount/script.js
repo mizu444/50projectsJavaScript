f8eaa67cf89883522ae5423a4b69c788f4fb7a43
Mizu Cuchty <m.cuchty@gmail.com>
2021-01-15 15:35:08 +0100

    user filter

--- verifyAccount/script.js
+++ verifyAccount/script.js
@@ -4,11 +4,16 @@ codes[0].focus();
 
 codes.forEach((code, idx) => {
   code.addEventListener("keydown", (e) => {
+    
     if (e.key >= 0 && e.key <= 9) {
       codes[idx].value = "";
-      setTimeout(() => codes[idx + 1].focus(), 10);
+      const index = idx + 1 > 5 ? 5 : idx + 1
+      setTimeout(() => codes[index].focus(), 10);
     } else if (e.key === "Backspace") {
-      setTimeout(() => codes[idx - 1].focus(), 10);
+      const index = idx - 1 < 0 ? 0 : idx - 1
+      setTimeout(() => codes[index].focus(), 10);
+    } else {
+      e.preventDefault()
     }
   });
 });
